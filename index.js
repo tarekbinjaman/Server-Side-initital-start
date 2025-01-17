69605007f4e56100d54e805c4aa9079fa4fbd13c
tarekbinjaman <tarekbinjaman@gmail.com>
2025-01-18 01:02:26 +0600

    done

--- index.js
+++ index.js
@@ -5,9 +5,37 @@ const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 5000;
 const app = express();
 const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 // middlewares
+
+
+const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.vkwnn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.use(express.json());
 app.use(cors({origin: ['http://localhost:5173',], credentials: true}));
